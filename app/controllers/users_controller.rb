ae5951cfbbe7ae434820140c2ef591df2bb9f3c6
清水 雅人 <shimizumasato@AppleBeliever.local>
2018-12-24 15:59:52 +0900

    [finish]5-9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:success] = "successfully"
       redirect_to user_path(@user.id)
     else
       render :edit
